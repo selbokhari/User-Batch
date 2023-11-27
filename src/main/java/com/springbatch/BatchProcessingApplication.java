3e47bad1e9f39584408b37653aa849c0d351128b
EL BOKHARI Sohaib <sohaib.elbokhari@sully-group.fr>
2023-11-27 14:09:45 +0100

    init commit

---
+++ src/main/java/com/springbatch/BatchProcessingApplication.java
@@ -0,0 +1,79 @@
+package com.springbatch;
+
+import com.springbatch.entities.Person;
+import com.springbatch.steps.userjob.JobCompletionNotificationListener;
+import com.springbatch.steps.userjob.processor.PersonItemProcessor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+public class BatchProcessingApplication {
+
+    public static void main(String[] args) {
+        System.exit(SpringApplication.exit(SpringApplication.run(BatchProcessingApplication.class, args)));
+    }
+
+
+    @Bean
+    public Job importUserJob(JobRepository jobRepository, Step step1, JobCompletionNotificationListener listener) {
+        return new JobBuilder("importUserJob", jobRepository)
+                .listener(listener)
+                .start(step1)
+                .build();
+    }
+
+    @Bean
+    public DataSourceTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+    @Bean
+    public Step step1(JobRepository jobRepository, DataSourceTransactionManager transactionManager,
+                      FlatFileItemReader<Person> reader, PersonItemProcessor processor, JdbcBatchItemWriter<Person> writer) {
+        return new StepBuilder("step1", jobRepository)
+                .<Person, Person> chunk(3, transactionManager)
+                .reader(reader)
+                .processor(processor)
+                .writer(writer)
+                .build();
+    }
+
+    @Bean
+    public FlatFileItemReader<Person> reader() {
+        return new FlatFileItemReaderBuilder<Person>()
+                .name("personItemReader")
+                .resource(new ClassPathResource("sample-data.csv"))
+                .delimited()
+                .names("firstName", "lastName")
+                .targetType(Person.class)
+                .build();
+    }
+
+    @Bean
+    public PersonItemProcessor processor() {
+        return new PersonItemProcessor();
+    }
+
+    @Bean
+    public JdbcBatchItemWriter<Person> writer(DataSource dataSource) {
+        return new JdbcBatchItemWriterBuilder<Person>()
+                .sql("INSERT INTO people (first_name, last_name) VALUES (:firstName, :lastName)")
+                .dataSource(dataSource)
+                .beanMapped()
+                .build();
+    }
+}
---
+++ src/main/java/com/springbatch/entities/Person.java
@@ -0,0 +1,4 @@
+package com.springbatch.entities;
+
+public record Person(String firstName, String lastName) {
+}
